81e204ebc46ea558249896bd8f28bb30886cbede
henelllis <henelllis>
2019-11-11 08:29:37 -0500

    play with robot methods

--- Chapter7/robot.ts
+++ Chapter7/robot.ts
@@ -1,4 +1,48 @@
 import {buildGraph, roads} from './notes';
-
+import VillageState from './village'
 
 const roadGraph = buildGraph(roads);
+
+function runRobot(state, robot,  memory ){
+    for (let turn = 0;; turn++){
+        if(state.parcels.length === 0){
+            console.log(`Done in ${turn} turns`);
+            break;
+        }
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+        console.log(`Moved to ${action.direction}`);
+    }
+}
+
+export function randomPick(array){
+    let choice = Math.floor(Math.random() * array.length);
+    return array[choice];
+}
+
+function randomRobot(state){
+    return {
+        direction: randomPick(roadGraph[state.place])
+    }
+}
+
+function villageFactory(parcelCount){
+
+    let parcels = [];
+    for ( let i = 0; i < parcelCount ; i++){
+        let address = randomPick(Object.keys(roadGraph));
+        let place;
+        do{
+            place = randomPick(Object.keys(roadGraph));
+        } while ( place === address);
+
+        parcels.push({ place, address});
+    }
+
+    return new VillageState("Post Office", parcels);
+}
+
+const myVillage = villageFactory(5);
+console.log(`myVillage ${JSON.stringify(myVillage.parcels)}`)
+// runRobot(villageFactory(5), randomRobot, null);
